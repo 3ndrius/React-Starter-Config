e6577784ba5e1f062b0018a26d32baea6d53b1ae
3ndrius <andrzejo1991@gmail.com>
2018-04-23 18:22:15 +0200

    State change dom

--- src/components/Go.js
+++ src/components/Go.js
@@ -8,24 +8,36 @@ class Go extends Component {
     super();
     this.state = {
       todos:['wash up', "make some food", "go somewhere"],
-      age: 30
-    }
+      age: 30,
+      status: 0
+    };
+    setTimeout(() => {
+      this.setState({
+        status:1
+      });
+    }, 3000);
+
    }
 
    tick() {
     this.setState({
-      age: 44
+      age: this.state.age +3
     });
   }
+
+ 
 
   render() {
     return (
       <div>
         <h2> {this.state.age} </h2>
+        <h4> {this.state.status} </h4>
         
             <h2> { this.state.todos[0]} </h2> 
             <h2> { this.state.todos[1]} </h2> 
             <h2> { this.state.todos[2]} </h2> 
+
+            <button onClick={() => this.tick() } > Make me older </button> 
       
       </div>
     );
