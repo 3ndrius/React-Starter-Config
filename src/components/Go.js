1090838d1cda55eb1d97aa91883d0cf721e49b08
3ndrius <andrzejo1991@gmail.com>
2018-04-23 19:09:36 +0200

    Cycle through data

--- src/components/Go.js
+++ src/components/Go.js
@@ -7,7 +7,7 @@ class Go extends Component {
   constructor(props) {
     super();
     this.state = {
-      todos:['wash up', "make some food", "go somewhere"],
+      todos:['wash up', "make some food", "go somewhere", "go to work"],
       age: 30,
       status: 0
     };
@@ -28,14 +28,22 @@ class Go extends Component {
  
 
   render() {
+
+    var todos = this.state.todos;
+    todos = todos.map(function(item, index){
+      return(
+        <li> {item}</li>
+      );
+    });
     return (
       <div>
         <h2> {this.state.age} </h2>
         <h4> {this.state.status} </h4>
         
-            <h2> { this.state.todos[0]} </h2> 
+            {/* <h2> { this.state.todos[0]} </h2> 
             <h2> { this.state.todos[1]} </h2> 
-            <h2> { this.state.todos[2]} </h2> 
+            <h2> { this.state.todos[2]} </h2>  */}
+            <ul> {todos} </ul>
 
             <button onClick={() => this.tick() } > Make me older </button> 
       
