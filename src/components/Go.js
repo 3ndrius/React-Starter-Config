153c53a427fb74700f196ac2dd38c2ec7c1f3c77
3ndrius <andrzejo1991@gmail.com>
2018-04-23 18:07:28 +0200

    Change state

--- src/components/Go.js
+++ src/components/Go.js
@@ -7,16 +7,25 @@ class Go extends Component {
   constructor(props) {
     super();
     this.state = {
-      todos:['wash up', "make some food", "go somewhere"]
+      todos:['wash up', "make some food", "go somewhere"],
+      age: 30
     }
    }
+
+   tick() {
+    this.setState({
+      age: 44
+    });
+  }
 
   render() {
     return (
       <div>
-            <h2> { this.state.todos[0]}   </h2> 
-            <h2> { this.state.todos[1]}   </h2> 
-            <h2> { this.state.todos[2]}   </h2> 
+        <h2> {this.state.age} </h2>
+        
+            <h2> { this.state.todos[0]} </h2> 
+            <h2> { this.state.todos[1]} </h2> 
+            <h2> { this.state.todos[2]} </h2> 
       
       </div>
     );
