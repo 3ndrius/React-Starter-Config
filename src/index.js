7479182d2b0f79391fd4dc597188257a23874510
3ndrius <andrzejo1991@gmail.com>
2018-04-24 11:30:40 +0200

    Clean

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import Go from './components/Go';
 import Todos from './components/Todos';
-import Todo from './components/Todo';
+
 
 
 
