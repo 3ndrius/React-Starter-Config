5febd749dd9eac86ba14cfd97c00f54fe468c2a8
3ndrius <andrzejo1991@gmail.com>
2018-04-26 14:10:57 +0200

    React manual config start

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,8 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>My React App!</h1>
-        <p> lorem ipsum dolor sir emmmet </p>
+        <h1>React starter setup</h1>
+        <p> React manual setting webpack, babel and other dependencies</p>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import Go from './components/Go';
-import Todos from './components/Todos';
 
 
 
 
 
 ReactDOM.render(<App />, document.getElementById('app'));
-ReactDOM.render(<Go /> , document.getElementById('go'));
-ReactDOM.render(<Todos /> , document.getElementById('todos'));
+
 
 
